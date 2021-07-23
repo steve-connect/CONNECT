1f34eb90a4e8bf9a0e08eb58bcf415ed14ee172e
steve-connect <86313114+steve-connect@users.noreply.github.com>
2021-07-22 21:38:26 -0400

    Update Game.jsx

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -797,7 +797,6 @@ function Game({match}) {
     return (
         <div>
             <div className='red-div'>
-                <h1 style={{color:'white'}}>TIME:</h1>
                 <h1 style={{color:'white'}} id='time'>{time}</h1>
             </div>
             <div id='cardContainer'></div>
