dcac3fa4f872c2620fc08514fd075751224b4925
steve-connect <86313114+steve-connect@users.noreply.github.com>
2021-07-31 10:07:26 +0900

    Update ClassSelection2.jsx

--- src/components/ClassSelection2.jsx
+++ src/components/ClassSelection2.jsx
@@ -20,46 +20,46 @@ function ClassSelection(props) {
         <h1>Select your Class</h1>
         <div className='class-selection'>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class0/0`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 0</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 0 (page 12)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class0b/0b`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 0b</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 0b (page 13)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class1/1`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 1</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 1 (page 18)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class2/2`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 2</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 2 (page 20)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class3/3`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 3</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 3 (page 26)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class4/4`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 4</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 4 (page 28)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class5/5`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 5</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 5 (page 34)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class6/6`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 6</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 6 (page 36)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class8/8`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 8</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 8 (page 46)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class9/9`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 9</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 9 (page 48)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class10/10`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 10</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 10 (page 54)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class11/11`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 11</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 11 (page 56)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class12/12`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 12</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 12 (page 62)</h1>
             </div>
             <div className='class-number' onClick={()=>{selectClass(`/start/${textbook}/class13/13`)}}>
-                <h1 style={{backgroundColor: color1, color:'white'}}>Class 13</h1>
+                <h1 style={{backgroundColor: color1, color:'white'}}>Class 13 (page 64)</h1>
             </div>
         </div>
         </>
